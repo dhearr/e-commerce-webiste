1286bbbac59a6ab4892fe67229733defc2c75e6d
dhearr <dhearr27@gmail.com>
2025-08-21 12:18:04 +0700

    feat(auth): user register system

--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -22,4 +22,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("core.urls")),
+    path("user/", include("userauths.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- ecommerce/userauths/views.py
+++ ecommerce/userauths/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from userauths.forms import UserRegisterForm
+from django.contrib import messages
+from django.contrib.auth import login, authenticate
 
-# Create your views here.
+
+def register_view(request):
+  if request.method == "POST":
+    form = UserRegisterForm(request.POST or None)
+    if form.is_valid():
+      new_user = form.save()
+      username = form.cleaned_data.get('username')
+      messages.success(request, f"Hey {username}, Your account has been created successfully!")
+      new_user = authenticate(email=form.cleaned_data['email'], password=form.cleaned_data['password1'])
+      login(request, new_user)
+      return redirect('core:index')
+  else:
+    form = UserRegisterForm()
+    
+  context = {
+    'form': form
+  }
+  return render(request, 'userauths/sign-up.html', context)
