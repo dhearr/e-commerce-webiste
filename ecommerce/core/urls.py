613ea9c0d4e48774d1990bf0e1601de1da3a51bf
dhearr <dhearr27@gmail.com>
2025-08-23 13:57:22 +0700

    feat(template): product list view

--- ecommerce/core/urls.py
+++ ecommerce/core/urls.py
@@ -5,5 +5,6 @@ app_name = "core"
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("product/", views.product_list_view, name="product-list"),
     path("category/", views.category_list_view, name="category-list"),
 ]
--- ecommerce/core/views.py
+++ ecommerce/core/views.py
@@ -14,6 +14,15 @@ def index(request):
     return render(request, "core/index.html", context)
 
 
+def product_list_view(request):
+    products = Product.objects.filter(product_status="published").order_by("-date")
+
+    context = {
+        "products": products,
+    }
+    return render(request, "core/product-list.html", context)
+
+
 def category_list_view(request):
     # categories = Category.objects.all()
     categories = Category.objects.all().annotate(product_count=Count("category"))
